8246e0ead75be324c044c1dbf8512ad524a56c6a
Shyan2 <49032507+Shyan2@users.noreply.github.com>
2023-10-18 16:18:07 +0800

    add blog

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { BlogStories } from '$lib/stores';
+</script>
+
+<h1>Blog</h1>
+
+<ul>
+	{#each $BlogStories as { id, title }}
+		<li><a href="/blog/{id}">{title}</a></li>
+	{/each}
+</ul>
---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { page } from '$app/stores';
+	import { BlogStories } from '$lib/stores';
+
+	$: id = $page.params.slug;
+	$: blog = $BlogStories.find((r) => String(r.id) === id);
+</script>
+
+<h1>{blog?.title}</h1>
+<div>{blog?.date}</div>
+<div>{@html blog?.text || ''}</div>
