cac95be61f567ca2d7d2c11dfe08fd44ef7730ba
Shyan2 <49032507+Shyan2@users.noreply.github.com>
2023-10-19 17:05:55 +0800

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,12 @@
 <script>
 </script>
 
-<div>Hi, this is the home page</div>
-<p>
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean faucibus vestibulum lectus. Proin aliquet vulputate
-	lacus quis aliquam. Mauris hendrerit ipsum et magna elementum dapibus. Duis est nisl, volutpat sagittis placerat eget,
-	tempor eget lorem. Maecenas sed neque ut purus laoreet ultricies eget vitae felis. Duis quis sollicitudin metus.
-	Vivamus nibh lectus, bibendum in quam quis, vulputate varius quam.
-</p>
-<p>
-	I am a software engineer specialized in developing and managing software applications for the construction industry.
-</p>
+<section>
+	<div>Senior BIM Applications Engineer | Software Engineer</div>
+	<div>Hi, my name is Shyan Chien</div>
+
+	<p>
+		I am a software engineer specialized in developing and managing software applications for the construction industry.
+	</p>
+	<p />
+</section>
