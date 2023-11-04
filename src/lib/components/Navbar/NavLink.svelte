852e4ab1fdb8e377227f41bd87403c61433ec5b0
Shyan2 <49032507+Shyan2@users.noreply.github.com>
2023-11-04 12:48:25 +0800

    Update NavLink.svelte

--- src/lib/components/Navbar/NavLink.svelte
+++ src/lib/components/Navbar/NavLink.svelte
@@ -5,8 +5,10 @@
 
 <a
 	{href}
-	class={`no-underline text-light-primary dark:text-dark-primary py-2 px-4 rounded transition ease-in hover:bg-dark-background hover:text-dark-primary dark:hover:bg-light-background dark:hover:text-light-primary focus:outline-none focus:bg-dark-background ${
-		isActive ? 'bg-dark-background text-dark-primary dark:bg-light-background dark:text-light-primary' : ''
+	class={`no-underline  dark:text-dark-primary py-2 px-4 rounded transition ease-in hover:bg-dark-background hover:text-dark-primary dark:hover:bg-light-background dark:hover:text-light-primary focus:outline-none focus:bg-dark-background ${
+		isActive
+			? 'bg-dark-background text-dark-primary dark:bg-light-background dark:text-light-primary'
+			: 'text-light-primary'
 	}`}
 >
 	<slot />
