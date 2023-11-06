ba0de654e8526cd9d2d7f888104fc13a2ef3ab07
Shyan2 <49032507+Shyan2@users.noreply.github.com>
2023-11-06 10:33:37 +0800

    update layout + footer

--- src/lib/components/Navbar/Navbar.svelte
+++ src/lib/components/Navbar/Navbar.svelte
@@ -8,42 +8,47 @@
 
 <nav class="mt-2 bg-navbar-light p-2 dark:bg-navbar-dark fixed top-0 left-0 w-full">
 	<div class="mx-auto max-w-4xl sm:px-6 lg:px-8 flex justify-between items-center">
-		<!-- Logo -->
-		<div class="flex-shrink-0">
-			<!-- <img class="h-6 w-auto" src="" alt="Your Company" /> -->
-			<p />
+		<!-- Logo Placeholder-->
+		<div class="flex-shrink-0 invisible">
+			<!-- <img class="h-6 w-auto" src="" alt="Logo" /> -->
+			<p class="h-6 w-auto">Logo</p>
 		</div>
 		<!-- Centered menu list -->
-		<ul class="list-none flex space-x-4">
-			<li><NavLink href="/" isActive={isCurrentPage('/')}>Home</NavLink></li>
-			<li><NavLink href="/about" isActive={isCurrentPage('/about')}>About</NavLink></li>
-			<!-- <li><NavLink href="/blog" isActive={isCurrentPage('/blog')}>Blog</NavLink></li> -->
-			<li><NavLink href="/contact" isActive={isCurrentPage('/contact')}>Contact</NavLink></li>
-			<!-- <li><NavLink href="/playground" isActive={isCurrentPage('/playground')}>Playground</NavLink></li> -->
-		</ul>
+		<div class="flex-grow">
+			<ul class="list-none flex justify-center space-x-4">
+				<li><NavLink href="/" isActive={isCurrentPage('/')}>Home</NavLink></li>
+				<li><NavLink href="/about" isActive={isCurrentPage('/about')}>About</NavLink></li>
+				<!-- <li><NavLink href="/blog" isActive={isCurrentPage('/blog')}>Blog</NavLink></li> -->
+				<li><NavLink href="/contact" isActive={isCurrentPage('/contact')}>Contact</NavLink></li>
+				<!-- <li><NavLink href="/playground" isActive={isCurrentPage('/playground')}>Playground</NavLink></li> -->
+			</ul>
+		</div>
+
 		<!-- Dark mode icon-->
-		<button
-			id="toggleDark"
-			class="text-blue-900 text-sm font-medium bg-blue-100 rounded-md"
-			style="background: transparent; border: none;"
-			on:click={toggleTheme}
-			><svg
-				viewBox="0 0 24 24"
-				xmlns="http://www.w3.org/2000/svg"
-				stroke-width="2"
-				stroke-linecap="round"
-				class="w-6 h-6"
-			>
-				<path
-					d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-					class="fill-light-background stroke-dark-background dark:stroke-light-background dark:fill-light-background"
-				/>
-				<path
-					d="M12 4v1M17.66 6.344l-.828.828M20.005 12.004h-1M17.66 17.664l-.828-.828M12 20.01V19M6.34 17.664l.835-.836M3.995 12.004h1.01M6 6l.835.836"
-					class=" stroke-dark-background dark:stroke-light-background"
-				/>
-			</svg>
-		</button>
+		<div class="flex-shrink-0">
+			<button
+				id="toggleDark"
+				class="text-blue-900 text-sm font-medium bg-blue-100 rounded-md"
+				style="background: transparent; border: none;"
+				on:click={toggleTheme}
+				><svg
+					viewBox="0 0 24 24"
+					xmlns="http://www.w3.org/2000/svg"
+					stroke-width="2"
+					stroke-linecap="round"
+					class="w-6 h-6"
+				>
+					<path
+						d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+						class="fill-light-background stroke-dark-background dark:stroke-light-background dark:fill-light-background"
+					/>
+					<path
+						d="M12 4v1M17.66 6.344l-.828.828M20.005 12.004h-1M17.66 17.664l-.828-.828M12 20.01V19M6.34 17.664l.835-.836M3.995 12.004h1.01M6 6l.835.836"
+						class=" stroke-dark-background dark:stroke-light-background"
+					/>
+				</svg>
+			</button>
+		</div>
 	</div>
 </nav>
 
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,8 +7,24 @@
 	onMount(() => onHydrated());
 </script>
 
-<Navbar />
-
-<main class="container max-w-4xl mx-auto px-2 mt-5 sm:px-6 lg:px-8">
-	<slot />
-</main>
+<div class="flex flex-col min-h-screen">
+	<Navbar />
+	<main class="flex-grow container max-w-4xl mx-auto px-2 mt-5 sm:px-6 lg:px-8">
+		<slot />
+	</main>
+	<footer class="mt-auto p-4">
+		<div class="text-center text-light-primary dark:text-dark-primary">
+			<p class="text-light-primary dark:text-dark-primary text-center text-sm">
+				Developed with <span class="text-red-500">&hearts;</span> using Svelte and Tailwind CSS
+			</p>
+			<p class="text-xs mt-4">
+				© 2023 Shyan Chien. Check out <a
+					class="underline hover:text-light-accent dark:text-dark-primary dark:hover:text-dark-accent transition-colors duration-200"
+					href="https://github.com/Shyan2/digital-me"
+					target="_blank"
+					rel="noopener noreferrer">GitHub Repository</a
+				> for more details.
+			</p>
+		</div>
+	</footer>
+</div>
